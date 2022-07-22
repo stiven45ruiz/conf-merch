483efe254210675d1072342a231907499e2d46ad
stiven45ruiz <stiven45ruiz@gmail.com>
2022-07-21 22:04:39 -0500

    webpackConfig

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+const hola = 'hola';
+
+import '../styles/components/app.scss';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Holis</h1>
+    </div>
+  );
+};
+
+export default App;
